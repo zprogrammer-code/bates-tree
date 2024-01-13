62d3263d5f94b65aea9c796673ced3ddbb08ff29
Zachary Jacobs <zprogrammercode@gmail.com>
2024-01-13 10:16:43 -0700

    I did this site in wordpress and now am trying to replicate the theme as fast as i can

---
+++ src/Components/Navbar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import"/home/zprogrammercode/BatesTree/bates-tree/src/App.css";
+
+
+
+
+const BtNav = () => {
+
+
+    return(
+        <>
+        <Navbar className="navbar-background">
+            <Container className="brand-container">
+                <Navbar.Brand href="#home">
+                    <img
+                     alt=""
+                     src="https://res.cloudinary.com/dlnshpfdv/image/upload/v1705162335/imageedit_1_4986758091_m8maes.png"
+                     width="150"
+                     height="61"
+                     className="d-inline-block align-top"
+                     />{' '}
+                </Navbar.Brand>
+            </Container>
+             <Container className="link-container">
+                 <Nav className="me-auto">
+                    <Nav.Link className="nav-links" href="#home">Home</Nav.Link>
+                     <Nav.Link className="nav-links" href="#features">Pricing</Nav.Link>
+                     <Nav.Link className="nav-links" href="#pricing">Blog</Nav.Link>
+                     <Nav.Link className="nav-links" href="#pricing">Contact</Nav.Link>
+                 </Nav>
+             </Container>
+        </Navbar>
+        </>
+    )
+}
+
+export default BtNav;

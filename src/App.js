583d3b910737aab00f9afd82f857e58225163165
Zachary Jacobs <zprogrammercode@gmail.com>
2024-01-13 23:36:17 -0700

    done with about 30 % of the home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import BtHero from './Components/Herosection';
 import BtNav from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <BtNav/>
+      <BtHero/>
     </div>
   );
 }
--- src/Components/Herosection.js
+++ src/Components/Herosection.js
@@ -7,13 +7,15 @@ const BtHero = () => {
 
 
     return(
-        <>
-        <Image src="" fluid>
-            <h1 className="big-hero-print"></h1>
-        </Image>
-        </>
+        <div className="herosection">
+            <h1 className="big-hero-print">Supplying Firewood, Ensuring Safety, Anytime, Anywhere</h1>
+            <h2 className="little-hero-print">Safety in Storms, Warmth in Winter: Bates Tree Removal Does It All.</h2>
+        </div>
 
     )
 }
 
 export default BtHero;
+
+/*        <Image src="https://ogden_images.s3.amazonaws.com/www.iamcountryside.com/images/sites/1/2019/03/19084309/AdobeStock_228778938-scaled-e1683315426520.jpeg" fluid/>
+*/

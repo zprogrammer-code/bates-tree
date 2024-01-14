c906853959214591b261d38699bd276da6a85664
Zachary Jacobs <zprogrammercode@gmail.com>
2024-01-13 21:05:18 -0700

    saving for the day

---
+++ src/Components/Herosection.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "/home/zprogrammercode/BatesTree/bates-tree/src/App.css";
+
+
+
+const BtHero = () => {
+
+
+    return(
+        <>
+        <Image src="" fluid>
+            <h1 className="big-hero-print"></h1>
+        </Image>
+        </>
+
+    )
+}
+
+export default BtHero;

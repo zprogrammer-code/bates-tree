4965897648aba124af1f61971364d26c167d0ec7
Zachary Jacobs <zprogrammercode@gmail.com>
2024-01-24 14:46:37 -0700

    First about section is underway

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import BtHero from './Components/Herosection';
 import BtNav from './Components/Navbar';
+import ServiceSection from './Components/Services';
 
 function App() {
   return (
     <div className="App">
       <BtNav/>
       <BtHero/>
+      <ServiceSection/>
     </div>
   );
 }
---
+++ src/Components/Services.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "/home/zprogrammercode/BatesTree/bates-tree/src/App.css";
+
+
+const ServiceSection = () => {
+
+
+
+    return(
+        <>
+        <section class="section-about">
+            <div className="u-center-text u-margin-bottom-8">
+                <h2 className="heading-secondary">
+                 Emergency Tree Removal and Fast Response Times
+              </h2>
+            </div>
+              <div className="row">
+                    <div className="col-1-of-2">
+                        <h3 className="heading-tertiary">You're goring to fall in love with nature</h3>
+                        <p className="paragraph">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste odio tempora inventore earum nam quaerat? Veritatis laudantium, officiis esse quibusdam natus totam quae, illo rem ipsam assumenda ipsa quasi? Sapiente.
+                        </p>
+                        <h3 className="heading-tertiary">Live adventures like you never have before</h3>
+                        <p className="paragraph">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste odio tempora inventore earum nam quaerat? Veritatis laudantium, officiis esse quibusdam natus totam quae, illo rem ipsam assumenda ipsa quasi? Sapiente.
+                        </p>
+                    </div>
+                    <div className="col-1-of-2">
+                        Image Composition
+                    </div>
+                </div>
+        
+    </section>
+    </>
+    )
+}
+
+export default ServiceSection;
